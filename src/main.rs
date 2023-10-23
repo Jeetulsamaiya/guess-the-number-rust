52079056e6812977e13032caa53a8da2f247fb92
jeetul samaiya <jeetul.samaiya@rapidops.com>
2023-10-23 14:26:20 +0530

    on invalid input it will continue without panic

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use std::io;
 use rand::prelude::*;
 use std::cmp::*;
+use std::io;
 fn main() {
     println!("Guess the Number");
     let random_number = thread_rng().gen_range(0..101);
     loop {
-    println!("please enter a number to guess");
-    let mut guess = String::new();
-    io::stdin().read_line( &mut guess).expect("failed to read line");
-    let guess : u32 = guess.trim().parse().expect("failed to parse");
-    println!("you guessed number {}", guess);
-    println!("random number is {}", random_number);
-    match guess.cmp(&random_number){
-        Ordering::Equal => {
-            println!("you win");
-            break;
-        },
-        Ordering::Greater => println!(" too big "),
-        Ordering::Less => println!("too small "),
-    }
+        println!("please enter a number to guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => {
+                println!("{}", number);
+                number
+            }
+            Err(err) => {
+                println!("invalid Input :-  {}, {}", err, guess);
+                continue;
+            }
+        };
+        println!("you guessed number {}", guess);
+        println!("random number is {}", random_number);
+        match guess.cmp(&random_number) {
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+            Ordering::Greater => println!(" too big "),
+            Ordering::Less => println!("too small "),
+        }
     }
 }
